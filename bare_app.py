0d774eb2393d73b52a27ed0b81e2e2478423f733
immerspeech <admin@immerspeech.com>
2025-04-28 16:40:13 +0900

    removed app.run

--- bare_app.py
+++ bare_app.py
@@ -70,7 +70,3 @@ def upload():
 @app.route("/processed/<filename>")
 def processed_file(filename):
     return send_from_directory(PROCESSED_FOLDER, filename)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
